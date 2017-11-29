4ecc8f2653ca1510d640f621ba78f41b15cdbc31
liuyongzhao <liuyongzhao@supcon.com>
2017-11-29 16:45:40 +0800

    cloud-client change to cloud-provider

--- src/main/java/com/lyz/cloud/client/consumer/HelloController.java
+++ src/main/java/com/lyz/cloud/client/consumer/HelloController.java
@@ -1,21 +1,27 @@
 package com.lyz.cloud.client.consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
+/*import org.slf4j.Logger;*/
+/*import org.slf4j.LoggerFactory;*/
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 	public class HelloController {
+	 	private static Logger logger = Logger.getLogger(HelloController.class);
+	    @Autowired
+	    private DiscoveryClient consumer;
 	    @Autowired
 	    private HelloService helloServcie;
-	    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
 	    @RequestMapping("/test/{fallback}")
 	    public String hello(@PathVariable("fallback") String fallback){
 	        String res=helloServcie.hello(fallback);
-	        logger.info("日志1：消费者浏览器发出请求");
+	        ServiceInstance instance = consumer.getLocalServiceInstance();
+	        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + res + " 日志1************");
 	        return "调用服务结果为"+res;
 	    }
 }
--- src/main/java/com/lyz/cloud/client/consumer/HelloService.java
+++ src/main/java/com/lyz/cloud/client/consumer/HelloService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("cloud-client")
+@FeignClient("cloud-provider")
 public interface HelloService {
 	 @RequestMapping("/hello/{fallback}")
 	    public String hello(@PathVariable("fallback") String fallback);
